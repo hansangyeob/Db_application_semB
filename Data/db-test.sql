aa15f348c1bb646e69d687609a986211da13146d
JooJeonghyeon99 <s3865746@rmit.edu.vn>
2021-09-07 18:05:57 +0900

    Add query for bidding

--- Data/db-test.sql
+++ Data/db-test.sql
@@ -31,7 +31,8 @@ create table customer_account (
     foreign key (registered_branch) references branch(b_code)
 )    Engine = InnoDB;
 
-CREATE INDEX idx_balance ON customer_account(balance ASC);
+
+
 
 CREATE TABLE auction_product (
   `p_id` mediumint(8) unsigned NOT NULL auto_increment,
@@ -46,8 +47,6 @@ CREATE TABLE auction_product (
   foreign key (seller) references customer_account(i_num),
   foreign key (buyer) references customer_account(i_num)
 ) AUTO_INCREMENT=1;
-
-CREATE INDEX idx_price_min ON auction_product(price_min ASC);
 
 create table admin(
     a_email varchar(255),
@@ -159,3 +158,29 @@ INSERT INTO bids (product_id,bidder,b_id,offer_price,offer_time) VALUES (7,3,6,4
 INSERT INTO bids (product_id,bidder,b_id,offer_price,offer_time) VALUES (8,12,4,8.38,'2020-09-26 02:08:38');
 INSERT INTO bids (product_id,bidder,b_id,offer_price,offer_time) VALUES (9,1,5,9.32,'2022-03-14 02:54:21');
 INSERT INTO bids (product_id,bidder,b_id,offer_price,offer_time) VALUES (10,8,10,1.42,'2022-09-06 01:48:48');
+
+-- Create index on [customer_account]
+CREATE INDEX idx_balance ON customer_account(balance ASC);
+
+-- Create index on [auction_product]
+CREATE INDEX idx_price_min ON auction_product(price_min ASC);
+
+-- Check out whether the query is performed with index
+ALTER TABLE customer_account DROP INDEX idx_balance;
+SHOW INDEX FROM customer_account;
+EXPLAIN
+    SELECT balance FROM customer_account;
+
+-- Show products' closing time ordered by ASC
+SELECT B.b_id,A.p_name,A.closing_time
+FROM auction_product A,bids B
+WHERE A.p_id = B.b_id
+ORDER BY A.closing_time ASC;
+
+
+-- Show maximum bid price ordered by DESC
+SELECT B.b_id,A.p_name,B.offer_price
+FROM bids B, auction_product A
+WHERE B.product_id=A.p_id
+ORDER BY B.offer_price DESC;
+
