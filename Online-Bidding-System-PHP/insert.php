b489524f4b0c3c20ad3f203b72d64ba4d49dcbe0
Tony Han <57244611+hansangyeob@users.noreply.github.com>
2021-09-12 19:52:37 +0700

    Delete insert.php

--- Online-Bidding-System-PHP/insert.php
+++
@@ -1,9 +0,0 @@
-<?php
-require './vendor/autoload.php'; // include Composer's autoloader
-$client = new MongoDB\Client("mongodb://localhost:27017");
-echo "connected to mondgo database!" . "<br>";
-$collection = $client->product_mongo->product;
-echo "Mongodb Databases selected";
-
-?>
-
